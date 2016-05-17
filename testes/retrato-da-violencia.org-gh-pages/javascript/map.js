39ed3f2947aefe7010e19dab238dababe3b19654
MarcosGrzeca <marcos-a-g@hotmail.com>
2016-05-16 22:32:06 -0300

    Mapa

--- testes/retrato-da-violencia.org-gh-pages/javascript/map.js
+++ testes/retrato-da-violencia.org-gh-pages/javascript/map.js
@@ -20,8 +20,8 @@ Map = (function ($) {
 
   function _setupCallbacks() {
     d3.selectAll('path.str3')
-      .on('mouseover', _hoverRegion)
-      .on('click', _selectRegion);
+    .on('mouseover', _hoverRegion)
+    .on('click', _selectRegion);
   };
 
   function _hoverRegion() {
@@ -35,94 +35,98 @@ Map = (function ($) {
 
   function _selectRegion() {
     var id = this.id,
-        codigo = id.replace(/.*_/, '');
+    codigo = id.replace(/.*_/, '');
 
     _classOnlyThisAs(id, 'active');
     _draw_timeline(codigo);
     _showInfo(codigo);
     window.location.hash = $.slug(Estupros[codigo].nome);
     if (_gaq) { _gaq.push(['_trackPageview', window.location.pathname +
-                                             window.location.hash]); }
+     window.location.hash]); }
   };
 
-  function _showInfo(codigo) {
-    var regiao = Estupros[codigo];
-    if (!regiao) { return; }
+function _showInfo(codigo) {
+  var regiao = Estupros[codigo];
+  if (!regiao) { return; }
 
-    var day = Math.round(100 * regiao.pela_manha / regiao.ocorrencias),
-        night = 100 - day,
-        ranking = _keysSortedByOpacity().indexOf(codigo) + 1,
-        proporcao = regiao.proporcao,
-        home = Math.round((100 * regiao.local.residencia) / regiao.ocorrencias),
-        street = Math.round((100 * regiao.local.via_publica) / regiao.ocorrencias),
-        others = 100 - home - street;
+  var day = Math.round(100 * regiao.pela_manha / regiao.ocorrencias),
+  night = 100 - day,
+  ranking = _keysSortedByOpacity().indexOf(codigo) + 1,
+  proporcao = regiao.proporcao,
+  home = Math.round((100 * regiao.local.residencia) / regiao.ocorrencias),
+  street = Math.round((100 * regiao.local.via_publica) / regiao.ocorrencias),
+  others = 100 - home - street;
 
-    $('#info h3').text(regiao.nome);
-    $('.population em').text(_formatNumber(regiao.populacao));
-    $('.victim em').text(regiao.media_idade_vitima);
-    $('.author em').text(regiao.media_idade_autor);
-    $('.night em').text(night+'%');
-    $('.day em').text(day+'%');
-    $('.home em').text(home+'%');
-    $('.street em').text(street+'%');
-    $('.others em').text(others+'%');
-    $('.ranking em').text(ranking+'ª');
-    $('.proporcao em').text(proporcao);
-  };
+  $('#info h3').text(regiao.nome);
+  $('.population em').text(_formatNumber(regiao.populacao));
+  $('.victim em').text(regiao.media_idade_vitima);
+  $('.author em').text(regiao.media_idade_autor);
+  $('.night em').text(night+'%');
+  $('.day em').text(day+'%');
+  $('.home em').text(home+'%');
+  $('.street em').text(street+'%');
+  $('.others em').text(others+'%');
+  $('.ranking em').text(ranking+'ª');
+  $('.proporcao em').text(proporcao);
+};
 
-  function _formatNumber(number) {
-    return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-  };
+function _formatNumber(number) {
+  return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+};
 
-  function _loadEstupros(callback) {
-    $.getJSON('../data/dados_estupros2.json', function (data) {
-      Estupros = data;
-      callback();
-    });
-  };
+function _loadEstupros(callback) {
+  $.getJSON('../data/dados_estupros.json', function (data) {
+    Estupros = data;
+    callback();
+  });
+};
 
-  function _focusInto(slug) {
-    var element;
+function _focusInto(slug) {
+  var element;
 
-    for (id in Estupros) {
-      if ($.slug(Estupros[id].nome) == slug) {
-        element = document.getElementById('svg_'+id);
-        break;
-      }
+  for (id in Estupros) {
+    if ($.slug(Estupros[id].nome) == slug) {
+      element = document.getElementById('micro_'+id);
+      break;
     }
+  }
 
-    if (!element) { return; }
+  if (!element) { return; }
 
-    d3.select(element).on('click').call(element);
-  };
+  d3.select(element).on('click').call(element);
+};
 
-  function _colorRegions() {
-    d3.selectAll('path.str3')
-      .attr('style', function () {
-          var id = d3.select(this).attr('id').replace(/.*_/, '');
-          return 'fill-opacity: '+Estupros[id].range / 5;
-      })
-      .each(function () {
-          var d3Element = d3.select(this);
-          d3Element.classed(d3Element.attr('id'), true);
-      });
-
+function _colorRegions() {
+  d3.selectAll('path.str3')
+  .attr('style', function () {
+    var id = d3.select(this).attr('id').replace(/.*_/, '');
+    return 'fill-opacity: '+Estupros[id].range / 5;
+  })
+  .each(function () {
+    var d3Element = d3.select(this);
+    d3Element.classed(d3Element.attr('id'), true);
+  });
+    //console.log(o.ordinal(10));
+    $("#micro_430214").css("fill", "blue");
     d3.selectAll('.bar-graph li')
-      .each(function (id) {
-        var d3RegionMap = d3.select('path.'+this.classList[0]),
-            opacity = d3RegionMap.attr('style').replace(/.*: (.*);?/, '$1'),
-            span = d3.select(this).select('span');
-        span.attr('style', span.attr('style') + '; background-color: rgba(220,20,60,'+ opacity +') !important;');
-      });
+    .each(function (id) {
+      var d3RegionMap = d3.select('path.'+this.classList[0]),
+      opacity = d3RegionMap.attr('style').replace(/.*: (.*);?/, '$1'),
+      span = d3.select(this).select('span');
+      span.attr('style', span.attr('style') + '; background-color: rgba(220,20,60,'+ opacity +') !important;');
+    });
   };
 
   function _drawBars() {
     d3.select('.bar-graph').append('ul').selectAll('li')
-      .data(_keysSortedByOpacity()).enter().append('li')
-      .attr('class', function (id) { return 'svg_'+id; })
-      .html(_barInfo)
-      .on('mouseover', _hoverBar)
-      .on('click', _clickRegion);
+    .data(_keysSortedByOpacity()).enter().append('li')
+    .attr('class', function (id) { 
+        //console.log(id);
+        return 'micro_'+id;
+      })
+    .html(_barInfo)
+    .on('mouseover', _hoverBar)
+    .on('click', _clickRegion);
   };
 
   function _keysSortedByOpacity() {
@@ -131,19 +135,19 @@ Map = (function ($) {
     sortedKeys.sort(function (a, b) {
       return parseFloat(Estupros[b].opacity) - parseFloat(Estupros[a].opacity)
     });
-
+    console.info(sortedKeys);
     return sortedKeys;
   };
 
   function _barInfo(id) {
     var regiao = Estupros[id],
-        meter = "<span class='meter' style='width: "+regiao.opacity*100+"%'>"+regiao.nome+"</span>";
+    meter = "<span class='meter' style='width: "+regiao.opacity*100+"%'>"+regiao.nome+"</span>";
 
     return meter;
   }
 
   function _hoverBar(id) {
-    _classOnlyThisAs('svg_'+id, 'hover');
+    _classOnlyThisAs('micro_'+id, 'hover');
   };
 
   function _clickRegion(id) {
@@ -151,7 +155,7 @@ Map = (function ($) {
   };
 
   function _sendEventToRegion(id, eventName) {
-    var region = document.getElementById('svg_'+id);
+    var region = document.getElementById('micro_'+id);
     d3.select(region).on(eventName).call(region);
   };
 
@@ -177,8 +181,8 @@ Map = (function ($) {
 })(jQuery);
 
 $(document).ready(function () {
-  //Map.initialize($('#map'), '../data/RioGrandedoSul_MesoMicroMunicip.svg');
-  Map.initialize($('#map'), '../data/Mapa_Rio_Grande_do_Sul.svg');
+  Map.initialize($('#map'), '../data/RioGrandedoSul_MesoMicroMunicip.svg');
+  //Map.initialize($('#map'), '../data/Mapa_Rio_Grande_do_Sul.svg');
 
   $("#cta").click(function(e) {
     e.preventDefault();
